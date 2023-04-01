e73e75688b838b24263db6832e4d0930aad627a1
olivierraude <olivierraude@gmail.com>
2023-03-31 20:31:50 -0400

    initialisation du premier commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/* var $cursor = $(".cursor"),
+  $overlay = $(".project-overlay");
+
+function moveCircle(e) {
+  TweenLite.to($cursor, 0.5, {
+    css: {
+      left: e.pageX,
+      top: e.pageY
+    },
+    delay: 0.03
+  });
+}
+
+$(".p-1").hover(function() {
+  $(".cursor").css({ "background-image": "url(img/image-1.jpg)" });
+});
+$(".p-2").hover(function() {
+  $(".cursor").css({ "background-image": "url(img/image-2.jpg)" });
+});
+$(".p-3").hover(function() {
+  $(".cursor").css({ "background-image": "url(img/image-3.jpg)" });
+});
+$(".p-4").hover(function() {
+  $(".cursor").css({ "background-image": "url(img/image-4.jpg)" });
+});
+
+var flag = false;
+$($overlay).mousemove(function() {
+  flag = true;
+  TweenLite.to($cursor, 0.3, { scale: 1, autoAlpha: 1 });
+  $($overlay).on("mousemove", moveCircle);
+});
+
+$($overlay).mouseout(function() {
+  flag = false;
+  TweenLite.to($cursor, 0.3, { scale: 0.1, autoAlpha: 0 });
+}); */
+
+/** VANILLA */
+
+let cursor = document.querySelector(".cursor"),
+overlay = document.querySelector(".project-overlay");
+projects = document.querySelectorAll(".project");
+
+function moveCircle(e) {
+  TweenLite.to(cursor, 0.5, {
+    css: {
+      left: e.pageX,
+      top: e.pageY
+    },
+    delay: 0.03
+  });
+}
+
+  document.querySelector(".p-1").addEventListener("mouseover", () => {
+    cursor.style.backgroundImage = ("url(img/image-1.jpg")
+  }) 
+  document.querySelector(".p-2").addEventListener("mouseover", () => {
+    cursor.style.backgroundImage = ("url(img/image-2.jpg)")
+    cursor.style.opacity = 0
+    console.log(cursor)
+  }) 
+  document.querySelector(".p-3").addEventListener("mouseover", () => {
+    cursor.style.backgroundImage = ("url(img/image-3.jpg)")
+  }) 
+  document.querySelector(".p-4").addEventListener("mouseover", () => {
+    cursor.style.backgroundImage = ("url(img/image-4.jpg)")
+  }) 
+  
+  /* projects.forEach((project, ind) => {
+    console.log("Test!!!")
+    ind++
+    project.addEventListener("mouseover", () => {
+        cursor.style.backgroundImage = (`url(img/image-${ind}.jpg)`)
+    })
+  }); */
+
+let flag = false;
+overlay.addEventListener("mousemove",() => {
+  flag = true;
+  TweenLite.to(cursor, 0.3, { scale: 1, autoAlpha: 1 });
+  overlay.addEventListener("mousemove", moveCircle);
+});
+
+overlay.addEventListener("mouseout",() => {
+  flag = false;
+  TweenLite.to(cursor, 0.3, { scale: 0.1, autoAlpha: 0 });
+});
